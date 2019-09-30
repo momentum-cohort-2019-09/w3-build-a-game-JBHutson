2315ef019814641e0b9bb2b9bffdd617a4e18656
JBHutson <hutsonjb@gmail.com>
2019-09-30 10:33:46 -0400

    added body off screen deletion

--- classes.js
+++ classes.js
@@ -7,15 +7,121 @@ class playerSquare{
     }
 
     update(){
-        if (this.keyboarder.isDown(37)) {
-            this.x -= 2
-          } else if (this.keyboarder.isDown(39)) {
-            this.x += 2
-          }
+        if (this.keyboarder.isDown(39)){
+            this.moveRight();
+        } else if (this.keyboarder.isDown(37)){
+            this.moveLeft();
+        } else if (this.keyboarder.isDown(38)){
+            this.moveUp();
+        } else if (this.keyboarder.isDown(40)){
+            this.moveDown();
+        }
+    }
+
+    moveUp(){
+        if (this.y != 150){
+            this.y -= 5;
+        }
+    }
+
+    moveDown(){
+        if (this.y != 410){
+            this.y +=5;
+        }
+    }
+
+    moveLeft(){
+        if (this.x != 150){
+            this.x -= 5;
+        }
+    }
+
+    moveRight(){
+        if (this.x != 410){
+            this.x += 5;
+        }
     }
 
     getColor(){
         return 'blue';
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getWidth(){
+        return this.w;
+    }
+}
+
+class BadSquare {
+    constructor(initX, initY, speed, direction){
+        this.x = initX;
+        this.y = initY;
+        this.s = speed;
+        this.d = direction;
+        this.c = 'red'
+        this.w = 20;
+    }
+
+    update(){
+        if (this.d == 'right'){
+            this.x += this.s;
+        } else if (this.d == 'left'){
+            this.x -= this.s;
+        } else if (this.d == 'down'){
+            this.y += this.s;
+        } else if (this.d == 'up'){
+            this.y -= this.s;
+        }
+    }
+
+    getColor(){
+        return this.c;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getWidth(){
+        return this.w;
+    }
+}
+
+class ReallyBadSquare {
+    constructor(initX, initY, speed, direction){
+        this.x = initX;
+        this.y = initY;
+        this.s = speed;
+        this.d = direction;
+        this.c = 'green';
+        this.w = 20;
+    }
+
+    update(){
+        if (this.d == 'right'){
+            this.x += this.s;
+        } else if (this.d == 'left'){
+            this.x -= this.s;
+        } else if (this.d == 'down'){
+            this.y += this.s;
+        } else if (this.d == 'up'){
+            this.y -= this.s;
+        }
+    }
+
+    getColor(){
+        return this.c;
     }
 
     getY(){
--- game.js
+++ game.js
@@ -8,13 +8,17 @@ class Game {
         }
         this.contextX = 150;
         this.contextY = 150;
+        this.count = 0;
         this.goodBoi = new playerSquare(this.contextX+130, this.contextY+130, 40);
+        this.reallyBadBois = [];
+        this.badBois = [];
+        this.requestId = undefined;
+        this.gameOver = false;
     }
 
     draw(){
         this.colorCanvas();
         this.drawArena();
-        this.drawGoodBoi();
     }
 
     colorCanvas(){
@@ -34,20 +38,145 @@ class Game {
         this.context.stroke();
     }
 
-    drawGoodBoi(){
-        this.context.fillStyle = this.goodBoi.getColor();
-        this.context.fillRect(this.goodBoi.getX(), this.goodBoi.getY(), this.goodBoi.getWidth(), this.goodBoi.getWidth());
+    drawBoi(boi){
+        this.context.fillStyle = boi.getColor();
+        this.context.fillRect(boi.getX(), boi.getY(), boi.getWidth(), boi.getWidth());
     }
 
+    addBadBoi(){
+        let potentialXVals = [0, 580, 1];
+        let potentialYVals = [0, 580];
+        let x = potentialXVals[this.getRandomInt(3)];
+        let y = 0;
+        if (x == 0){
+            y = this.randomIntFromInterval(150, 430);
+            let badBoi = new BadSquare(x, y, 10, 'right');
+            this.badBois.push(badBoi);
+        } else if (x == 580) {
+            y = this.randomIntFromInterval(150, 430);
+            let badBoi = new BadSquare(x, y, 10, 'left');
+            this.badBois.push(badBoi);
+        } else {
+            y = potentialYVals[this.getRandomInt(2)];
+            if (y == 0){
+                x = this.randomIntFromInterval(150, 430);
+                let badBoi = new BadSquare(x, y, 10, 'down');
+                this.badBois.push(badBoi);
+            } else if (y == 580){
+                x = this.randomIntFromInterval(150, 430);
+                let badBoi = new BadSquare(x, y, 10, 'up');
+                this.badBois.push(badBoi);
+            }
+        }
+    }
+
+    addReallyBadBoi(){
+        let potentialXVals = [0, 580, 1];
+        let potentialYVals = [0, 580];
+        let x = potentialXVals[this.getRandomInt(3)];
+        let y = 0;
+        if (x == 0 && this.count == 60){
+            y = this.randomIntFromInterval(150, 430);
+            let reallyBadBoi = new ReallyBadSquare(x, y, 15, 'right');
+            this.reallyBadBois.push(reallyBadBoi);
+            this.count = 0;
+        } else if (x == 580 && this.count == 60) {
+            y = this.randomIntFromInterval(150, 430);
+            let reallyBadBoi = new ReallyBadSquare(x, y, 15, 'left');
+            this.reallyBadBois.push(reallyBadBoi);
+            this.count = 0;
+        } else {
+            y = potentialYVals[this.getRandomInt(2)];
+            if (y == 0 && this.count == 60){
+                x = this.randomIntFromInterval(150, 430);
+                let reallyBadBoi = new ReallyBadSquare(x, y, 15, 'down');
+                this.reallyBadBois.push(reallyBadBoi);
+                this.count = 0;
+            } else if (y == 580 && this.count == 60){
+                x = this.randomIntFromInterval(150, 430);
+                let reallyBadBoi = new ReallyBadSquare(x, y, 15, 'up');
+                this.reallyBadBois.push(reallyBadBoi);
+                this.count = 0;
+            }
+        }
+    }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      }
+
     update(){
+        this.addBadBoi();
+        this.addReallyBadBoi();
+        this.addToCount();
         this.goodBoi.update();
-        this.drawGoodBoi();
+        this.drawBoi(this.goodBoi);
+        for (let i = 0; i<this.badBois.length; i++){
+            this.badBois[i].update();
+            this.drawBoi(this.badBois[i]);
+        }
+        for (let i = 0; i<this.reallyBadBois.length; i++){
+            this.reallyBadBois[i].update();
+            this.drawBoi(this.reallyBadBois[i]);
+        }
+        if (this.badBois.length > 0){
+            for (let i = 0; i < this.badBois.length; i++) {
+                if (!this.boiOnScreen(this.badBois[i])){
+                    this.badBois.shift();
+                }
+            }
+        }
+        if (this.reallyBadBois.length > 0){
+            for (let i = 0; i < this.reallyBadBois.length; i++) {
+                if (!this.boiOnScreen(this.reallyBadBois[i])){
+                    this.reallyBadBois.shift();
+                }
+            }
+        }
+        if (this.colliding()){
+            this.gameOver = true;
+        }
+    }
+
+    colliding(){
+        for (let i = 0; i< this.reallyBadBois.length; i++){
+            return !(
+                this.goodBoi.getX() + this.goodBoi.getWidth() / 2 < this.reallyBadBois[i].getX() - this.reallyBadBois[i].getWidth() / 2 ||
+                this.goodBoi.getY() + this.goodBoi.getWidth() / 2 < this.reallyBadBois[i].getY() - this.reallyBadBois[i].getWidth() / 2 ||
+                this.goodBoi.getX() - this.goodBoi.getWidth() / 2 > this.reallyBadBois[i].getX() + this.reallyBadBois[i].getWidth() / 2||
+                this.goodBoi.getY() - this.goodBoi.getWidth() / 2 > this.reallyBadBois[i].getY() + this.reallyBadBois[i].getWidth() / 2
+            );
+        };
+    }
+
+    boiOnScreen(boi){
+        let xOnScreen = false;
+        if (boi.getX() >= 0 && boi.getX() <= 600){
+            xOnScreen = true;
+        }
+        if (xOnScreen && boi.getY() >= 0 && boi.getY() <= 600){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    addToCount(){
+        this.count++;
     }
 
     tick(){
-        this.update();
-        this.draw();
-        requestAnimationFrame(this.tick());
+        let self = this;
+        self.draw();
+        self.update();
+
+        if (!this.gameOver){
+            this.requestId = requestAnimationFrame(function() {self.tick()});
+        }
     }
 }
 
